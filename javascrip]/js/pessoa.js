3bd89315514d1253d008428e9f2035e0dbfa9f21
Gabriel Soares <115032490+gasosi@users.noreply.github.com>
2024-07-31 11:52:37 -0300

    Add files via upload

---
+++ javascrip]/js/pessoa.js
@@ -0,0 +1,23 @@
+class Pessoa{
+    nome 
+    _cpf
+    email
+    _senha
+}
+let pessoa_1 = new Pessoa();
+let pessoa_2 = new Pessoa();
+
+
+pessoa_1.nome = "Gabriel";
+pessoa_1.cpf = "43996198825";
+pessoa_1.email = "soaresda.silva@escola.pr.gov.br";
+pessoa_1.senha = "banana6582";
+
+
+pessoa_2.nome = "Elon Musk";
+pessoa_2.cpf = "1401123456";
+pessoa_2.email = "musk.elon@escola.pr.gov.br";
+pessoa_2.senha = "ilovemarte";
+
+console.log(pessoa_1);
+console.log(pessoa_2);
---
+++ javascrip]/js/script.js
@@ -0,0 +1,18 @@
+function soma(num1,num2){
+    return num1+num2;
+}
+
+var resultado = soma(15,25);
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("objeto").innerHTML="<b>Resultado da soma: </b>" + resultado;
